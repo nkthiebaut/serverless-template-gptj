9db0e3c04be33caeeaa5c62c059fa7d275e6dd48
Nicolas <nkthiebaut@gmail.com>
2022-12-08 00:16:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def inference(model_inputs:dict) -> dict:
     input_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
 
     # Run the model
-    output = model.generate(input_tokens)
+    output = model.generate(input_tokens, max_new_tokens=128)
 
     # Decode output tokens
     output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
